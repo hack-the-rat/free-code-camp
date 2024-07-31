79193bc8f05464b680427087bfd19cb32c63620f
Robert Taylor <trobert844@gmail.com>
2024-07-30 20:36:59 -0400

    pyramid generator step 50

--- javascript-algorithms-data-structures/pyramid-generator/script.js
+++ javascript-algorithms-data-structures/pyramid-generator/script.js
@@ -2,10 +2,11 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-function padRow(call) {
+function padRow() {
 
 }
 const call = padRow();
+console.log(call);
 
 for (let i = 0; i < count; i = i + 1) {
     rows.push(character.repeat(i + 1));
