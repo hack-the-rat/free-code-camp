b864ea81f532ffaa08a7c27cd7893d91b780913e
Robert Taylor <trobert844@gmail.com>
2024-08-03 19:07:20 -0400

    pyramid generator step 108

--- javascript-algorithms-data-structures/pyramid-generator/script.js
+++ javascript-algorithms-data-structures/pyramid-generator/script.js
@@ -20,9 +20,11 @@ function padRow(rowNumber, rowCount) {
 } */
 
 const numbers = [1, 2, 3];
+const shifted = numbers.shift();
+console.log(shifted);
 const unshifted = numbers.unshift(5);
-console.log(numbers);
 console.log(unshifted);
+console.log(numbers);
 
 let result = "";
 
