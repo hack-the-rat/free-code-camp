22e9fef45f9c52484808332994d86012a9acf79d
Robert Taylor <trobert844@gmail.com>
2024-08-04 15:38:56 -0400

    gradebook app part 1

--- javascript-algorithms-data-structures/gradebook-app/script.js
+++ javascript-algorithms-data-structures/gradebook-app/script.js
@@ -1,5 +1,10 @@
 function getAverage(scores) {
-
+    let total = 0;
+    for (let i = 0; i < scores.length; i++) {
+        total += scores[i];
+    }
+    let average = total / scores.length;
+    return average;
 }
 
 console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
