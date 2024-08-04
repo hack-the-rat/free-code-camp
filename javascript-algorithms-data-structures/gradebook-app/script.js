35f92b9929a30682fcc69d1cfdaad72409c71f1c
Robert Taylor <trobert844@gmail.com>
2024-08-04 15:47:15 -0400

    gradebook app part 2

--- javascript-algorithms-data-structures/gradebook-app/script.js
+++ javascript-algorithms-data-structures/gradebook-app/script.js
@@ -1,11 +1,28 @@
 function getAverage(scores) {
-    let total = 0;
-    for (let i = 0; i < scores.length; i++) {
-        total += scores[i];
+    let sum = 0;
+  
+    for (const score of scores) {
+        sum += score;
     }
-    let average = total / scores.length;
-    return average;
+  
+    return sum / scores.length;
+}
+function getGrade(score) {
+    if (score === 100) {
+        return "A++";
+    } else if (score >= 90 && score < 100) {
+        return "A";
+    } else if (score >= 80 && score < 90) {
+        return "B";
+    } else if (score >= 70 && score < 80) {
+        return "C";
+    } else if (score >= 60 && score < 70) {
+        return "D";
+    } else {
+        return "F";
+    }
 }
 
-console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
-console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
+console.log(getGrade(96));
+console.log(getGrade(82));
+console.log(getGrade(56));
