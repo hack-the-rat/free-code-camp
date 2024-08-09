b8a41533e91400bb57c0c45d6794b002e91279fd
Robert Taylor <trobert844@gmail.com>
2024-08-09 08:17:15 -0400

    random background color changer part 5

--- javascript-algorithms-data-structures/random-background-color-changer/script.js
+++ javascript-algorithms-data-structures/random-background-color-changer/script.js
@@ -17,3 +17,6 @@ function getRandomIndex() {
 }
   
 const body = document.querySelector("body");
+const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
+
+console.log(bgHexCodeSpanElement);
