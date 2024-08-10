41f9e8c8cd12f5cce014832361e81a6f11cc484b
Robert Taylor <trobert844@gmail.com>
2024-08-10 13:02:59 -0400

    calorie counter step 63

--- javascript-algorithms-data-structures/calorie-counter/script.js
+++ javascript-algorithms-data-structures/calorie-counter/script.js
@@ -40,7 +40,7 @@ function getCaloriesFromInputs(list) {
         const invalidInputMatch = isInvalidInput(currVal);
 
         if (invalidInputMatch) {
-            
+            alert(`Invalid Input: ${invalidInputMatch[0]}`);
         }
     }
 }
