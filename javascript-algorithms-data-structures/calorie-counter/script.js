2f5307ac8a389dc671d70196b13bead286639c0c
Robert Taylor <trobert844@gmail.com>
2024-08-10 13:16:03 -0400

    calorie counter step 70

--- javascript-algorithms-data-structures/calorie-counter/script.js
+++ javascript-algorithms-data-structures/calorie-counter/script.js
@@ -36,7 +36,8 @@ function calculateCalories(e) {
     e.preventDefault();
     isError = false;
 
-    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]')
+    const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+    const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
 }
 
 function getCaloriesFromInputs(list) {
