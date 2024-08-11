e64921a23a2f4aff7d661552ff814d15dbc0140b
Robert Taylor <trobert844@gmail.com>
2024-08-11 16:13:35 -0400

    rock paper scissors game part 2

--- javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
+++ javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
@@ -1,6 +1,21 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-    const randomIndex = Math.floor(options.length * Math.random());
+    const randomIndex = Math.floor(Math.random() * options.length);
     return options[randomIndex];
 }
-console.log(getRandomComputerResult());
+  
+function hasPlayerWonTheRound(player, computer) {
+    if (player == "Rock" && computer == "Scissors") {
+        return true;
+    }
+    if (player == "Scissors" && computer == "Paper") {
+        return true;
+    }
+    if (player == "Paper" && computer == "Rock") {
+        return true;
+    }
+    return false;
+}
+  
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
