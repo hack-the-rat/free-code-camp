ac0709fe8bcb7e227b15cb17f79aa0f768ab864a
Robert Taylor <trobert844@gmail.com>
2024-08-11 16:56:36 -0400

    rock paper scissors game complete

--- javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
+++ javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
@@ -40,13 +40,42 @@ function showResults(userOption) {
     roundResultsMsg.innerText = getRoundResults(userOption);
     computerScoreSpanElement.innerText = computerScore;
     playerScoreSpanElement.innerText = playerScore;
-    if (playerScore >= 2) {
-        winnerMsgElement.innerText = "Player has won the game!";
-        resetGameBtn.style.display = "block";
-        optionsContainer.style.display = "none";
-    } else if (computerScore >= 2) {
-        winnerMsgElement.innerText = "Computer has won the game!";
+  
+    if (playerScore === 3 || computerScore === 3) {
+        winnerMsgElement.innerText = `${
+            playerScore === 3 ? "Player" : "Computer"
+        } has won the game!`;
+      
         resetGameBtn.style.display = "block";
         optionsContainer.style.display = "none";
     }
+  
 };
+function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+    resetGameBtn.style.display = "none";
+    optionsContainer.style.display = "block";
+    winnerMsgElement.innerText = "";
+    roundResultsMsg.innerText = "";
+};
+  
+resetGameBtn.addEventListener("click", resetGame);
+  
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+  
+rockBtn.addEventListener("click", function () {
+    showResults("Rock");
+});
+  
+paperBtn.addEventListener("click", function () {
+    showResults("Paper");
+});
+  
+scissorsBtn.addEventListener("click", function () {
+    showResults("Scissors");
+});
