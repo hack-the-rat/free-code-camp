b77a122d5f8db16c6e2d79c6744c954ba9ba6bab
Robert Taylor <trobert844@gmail.com>
2024-08-11 16:07:41 -0400

    rock paper scissors game part 1

--- javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
+++ javascript-algorithms-data-structures/rock-paper-scissors-game/script.js
@@ -1,5 +1,6 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
-  
-  }
-  console.log(getRandomComputerResult());
+    const randomIndex = Math.floor(options.length * Math.random());
+    return options[randomIndex];
+}
+console.log(getRandomComputerResult());
