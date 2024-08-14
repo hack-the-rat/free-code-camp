09e1e55849d6ef7d7b85cfd00a8d7ae6e45e8318
Robert Taylor <trobert844@gmail.com>
2024-08-14 17:32:11 -0400

    music player step 42

--- javascript-algorithms-data-structures/music-player/script.js
+++ javascript-algorithms-data-structures/music-player/script.js
@@ -123,6 +123,10 @@ const renderSongs = (array) => {
     playlistSongs.innerHTML = songsHTML;
 };
 
+playButton.addEventListener("click", () => {
+    
+});
+
 const sortSongs = () => {
     userData?.songs.sort((a, b) => {
         if (a.title < b.title) {
