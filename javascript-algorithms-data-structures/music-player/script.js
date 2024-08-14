03edffea848da28f7a8315e048d11e855a4a1c6c
Robert Taylor <trobert844@gmail.com>
2024-08-14 17:06:46 -0400

    music player step 35

--- javascript-algorithms-data-structures/music-player/script.js
+++ javascript-algorithms-data-structures/music-player/script.js
@@ -79,12 +79,15 @@ const allSongs = [
 ];
 
 const audio = new Audio();
-
 let userData = {
     songs: [...allSongs],
     currentSong: null,
     songCurrentTime: 0,
 };
+
+const playSong = id => {
+    
+}
 
 const renderSongs = (array) => {
     const songsHTML = array
