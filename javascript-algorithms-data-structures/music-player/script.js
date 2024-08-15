27938e408bcbfa15ea1a7a7cba54b21c86bdf90a
Robert Taylor <trobert844@gmail.com>
2024-08-14 21:32:26 -0400

    music player step 58

--- javascript-algorithms-data-structures/music-player/script.js
+++ javascript-algorithms-data-structures/music-player/script.js
@@ -119,7 +119,11 @@ const playNextSong = () => {
 };
 
 const playPreviousSong = () => {
-    
+    if (userData?.currentSong === null) {
+        return;
+    } else {
+        const currentSongIndex = getCurrentSongIndex();
+    }
 };
 
 const renderSongs = (array) => {
