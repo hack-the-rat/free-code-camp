f414c853a7c77c35d07046897aa8842da3e66d73
Robert Taylor <trobert844@gmail.com>
2024-09-09 16:58:48 -0400

    palindrome checker part 6

--- javascript-algorithms-data-structures/palindrome-checker/script.js
+++ javascript-algorithms-data-structures/palindrome-checker/script.js
@@ -1,3 +1,26 @@
 const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
+
+const checkForPalindrome = input => {
+    const originalInput = input;
+
+    if (input === '') {
+        alert('Please input a value');
+        return;
+    }
+
+    resultDiv.replaceChildren();
+
+    const lowerCaseStr = input.replace(/[^A-Za-z0-9]/gi, '').toLowerCase();
+    let resultMsg = `<strong>${originalInput}</strong> ${
+        lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'
+    } a palindrome.`;
+
+    const pTag = document.createElement('p');
+    pTag.className = 'user-input';
+    pTag.innerHTML = resultMsg;
+    resultDiv.appendChild(pTag);
+
+    resultDiv.classList.remove('hidden');
+};
