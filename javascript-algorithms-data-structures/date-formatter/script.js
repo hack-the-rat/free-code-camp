749eefc6503064be4642174645e05c3cc7469be2
Robert Taylor <trobert844@gmail.com>
2024-09-11 17:45:22 -0400

    date formatter step 18

--- javascript-algorithms-data-structures/date-formatter/script.js
+++ javascript-algorithms-data-structures/date-formatter/script.js
@@ -11,6 +11,9 @@ const minutes = date.getMinutes();
 const formattedDate = `${day}-${month}-${year}`;
 currentDateParagraph.textContent = formattedDate;
 
+const exampleSentence = "selur pmaCedoCeerf".split();
+console.log(exampleSentence);
+
 dateOptionsSelectElement.addEventListener("change", () => {
     switch (dateOptionsSelectElement.value) {
         case "yyyy-mm-dd":
