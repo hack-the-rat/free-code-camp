4343aae8c92cdf7a0951b4e74314e2dfe3087ff4
Robert Taylor <trobert844@gmail.com>
2024-09-13 17:13:24 -0400

    football team cards set step 8

--- javascript-algorithms-data-structures/football-team-cards/script.js
+++ javascript-algorithms-data-structures/football-team-cards/script.js
@@ -9,5 +9,8 @@ const myFavoriteFootballTeam = {
     sport: "Football",
     year: 1986,
     isWorldCupWinner: true,
-    headCoach: {}
+    headCoach: {
+        coachName: "Carlos Bilardo",
+        matches: 7
+    }
 };
