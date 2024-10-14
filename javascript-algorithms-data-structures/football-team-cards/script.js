624e74672dcd22812c145526d302b3fb619fe94c
Robert Taylor <trobert844@gmail.com>
2024-10-14 14:36:33 -0400

    football team cards set step 37

--- javascript-algorithms-data-structures/football-team-cards/script.js
+++ javascript-algorithms-data-structures/football-team-cards/script.js
@@ -198,4 +198,8 @@ const setPlayerCards = (arr = players) => {
 
 playersDropdownList.addEventListener("change", (e) => {
     playerCards.innerHTML = "";
+
+    switch (e.target.value) {
+        
+    }
 });
