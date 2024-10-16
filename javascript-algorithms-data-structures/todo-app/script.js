8517c4df63871055ad3040c37ff124e69b71ce4f
Robert Taylor <trobert844@gmail.com>
2024-10-15 21:07:10 -0400

    todo app step 12

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -30,5 +30,8 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
+
     const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {};
+    console.log(taskObj);
 });
