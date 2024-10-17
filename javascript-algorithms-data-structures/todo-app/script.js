99328cd344e4069632875c566819de971fb836f1
Robert Taylor <trobert844@gmail.com>
2024-10-17 18:27:55 -0400

    todo app step 26

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -46,7 +46,8 @@ taskForm.addEventListener("submit", (e) => {
     taskData.forEach(({id, title, date, description}) => {
         tasksContainer.innerHTML += `
             <div class="task" id="${id}">
-                <p><strong>Title:</strong>${title}</p>
+                <p><strong>Title:</strong> ${title}</p>
+                <p><strong>Date:</strong> ${date}</p>
             </div>
         `;
     });
