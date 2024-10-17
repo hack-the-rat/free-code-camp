ee4dae95a834f923cd8fd7470bc13187a8bbbaf9
Robert Taylor <trobert844@gmail.com>
2024-10-17 18:09:25 -0400

    todo app step 22

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -42,4 +42,6 @@ taskForm.addEventListener("submit", (e) => {
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
+
+    taskData.forEach(({id, title, date, description}));
 });
