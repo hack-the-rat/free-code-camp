06f67b91cac769794f36a8b0af77b5c6735abc75
Robert Taylor <trobert844@gmail.com>
2024-10-21 15:10:17 -0400

    todo app step 30

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -13,6 +13,10 @@ const descriptionInput = document.getElementById('description-input');
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+    
+};
+
 openTaskFormBtn.addEventListener("click", () =>
     taskForm.classList.toggle("hidden")
 );
