6df1135cc4ac084a925b311860e7c0d5d6a6b30c
Robert Taylor <trobert844@gmail.com>
2024-10-22 14:08:42 -0400

    todo app step 35

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -26,8 +26,12 @@ openTaskFormBtn.addEventListener("click", () =>
 );
 
 closeTaskFormBtn.addEventListener("click", () => {
-    confirmCloseDialog.showModal();
     const formInputsContainValues = titleInput.value || dateInput.value || descriptionInput.value;
+    if (formInputsContainValues) {
+        confirmCloseDialog.showModal();
+    } else {
+        reset();
+    }
 });
 
 cancelBtn.addEventListener("click", () => confirmCloseDialog.close());
