755adf4772ba05ee8e183effb48ee10e20c7475b
Robert Taylor <trobert844@gmail.com>
2024-11-01 16:16:54 -0400

    todo app step 48

--- javascript-algorithms-data-structures/todo-app/script.js
+++ javascript-algorithms-data-structures/todo-app/script.js
@@ -57,9 +57,14 @@ const deleteTask = (buttonEl) => {
 };
 
 const editTask = (buttonEl) => {
-    const dataArrIndex = taskData.findIndex((item) => item.id === buttonEl.parentElement.id);
+    const dataArrIndex = taskData.findIndex(
+        (item) => item.id === buttonEl.parentElement.id
+    );
 
     currentTask = taskData[dataArrIndex];
+    titleInput.value = currentTask.title;
+    dateInput.value = currentTask.date;
+    descriptionInput.value = currentTask.description;
 };
 
 const reset = () => {
