3bc84b6c4c87df611c4657a0d38d00c4556fb8bd
Robert Taylor <trobert844@gmail.com>
2024-11-19 17:07:28 -0500

    decimal binary converter step 91

--- javascript-algorithms-data-structures/decimal-binary-converter/script.js
+++ javascript-algorithms-data-structures/decimal-binary-converter/script.js
@@ -29,7 +29,9 @@ const showAnimation = () => {
     result.innerText = "Call Stack Animation";
 
     animationData.forEach((obj) => {
-        
+        setTimeout(() => {
+
+        }, obj.addElDelay);
     });
 };
 
