a8f3793ea1cbdf4e8e2af446fa6715bf487c5ade
Robert Taylor <trobert844@gmail.com>
2024-11-19 13:15:52 -0500

    decimal binary converter step 88

--- javascript-algorithms-data-structures/decimal-binary-converter/script.js
+++ javascript-algorithms-data-structures/decimal-binary-converter/script.js
@@ -26,15 +26,7 @@ const decimalToBinary = (input) => {
 };
 
 const showAnimation = () => {
-    setTimeout(() => {
-        console.log("free");
-    }, 500);
-    setTimeout(() => {
-        console.log("Code");
-    }, 1000);
-    setTimeout(() => {
-        console.log("Camp");
-    }, 1500);
+    
 };
 
 const checkUserInput = () => {
