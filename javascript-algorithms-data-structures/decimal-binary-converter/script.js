934511a82dc76863e16eb52b8923cb52901eec75
Robert Taylor <trobert844@gmail.com>
2024-11-29 13:53:46 -0500

    decimal binary converter step 106

--- javascript-algorithms-data-structures/decimal-binary-converter/script.js
+++ javascript-algorithms-data-structures/decimal-binary-converter/script.js
@@ -51,7 +51,7 @@ const showAnimation = () => {
         }, obj.showMsgDelay);
 
         setTimeout(() => {
-
+            document.getElementById(obj.inputVal).remove();
         }, obj.removeElDelay);
     });
 };
