d13fc49f784984b117f92f9a49a3cd0c75f4fbd0
Robert Taylor <trobert844@gmail.com>
2024-12-02 14:10:42 -0500

    roman numeral converter part 2

--- javascript-algorithms-data-structures/roman-numeral-converter/script.js
+++ javascript-algorithms-data-structures/roman-numeral-converter/script.js
@@ -1,3 +1,31 @@
 const form = document.getElementById('form');
 const convertButton = document.getElementById('convert-btn');
 const output = document.getElementById('output');
+
+const convertToRoman = (num) => {
+    const ref = [
+        ['M', 1000],
+        ['CM', 900],
+        ['D', 500],
+        ['CD', 400],
+        ['C', 100],
+        ['XC', 90],
+        ['L', 50],
+        ['XL', 40],
+        ['X', 10],
+        ['IX', 9],
+        ['V', 5],
+        ['IV', 4],
+        ['I', 1]
+    ];
+    const res = [];
+
+    ref.forEach((arr) => {
+        while (num >= arr[1]) {
+            res.push(arr[0]);
+            num -= arr[1];
+        }
+    });
+
+    return res.join('');
+};
