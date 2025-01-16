f9a75ebf40c041e0e0e02f5566fc68eef37063d7
Robert Taylor <trobert844@gmail.com>
2025-01-16 14:58:02 -0500

    spam filter step 4

--- javascript-algorithms-data-structures/spam-filter/script.js
+++ javascript-algorithms-data-structures/spam-filter/script.js
@@ -2,6 +2,10 @@ const messageInput = document.getElementById("message-input");
 const result = document.getElementById("result");
 const checkMessageButton = document.getElementById("check-message-btn");
 
+const isSpam = (msg) => {
+    return false;
+};
+
 checkMessageButton.addEventListener("click", () => {
     if (messageInput.value === "") {
         alert("Please enter a message.");
