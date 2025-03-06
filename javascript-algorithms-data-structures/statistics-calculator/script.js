64ae64ef992f929714ab2834b86b5c4a8ab7d603
Robert Taylor <trobert844@gmail.com>
2025-03-06 18:32:20 -0500

    statistics calculator step 41

--- javascript-algorithms-data-structures/statistics-calculator/script.js
+++ javascript-algorithms-data-structures/statistics-calculator/script.js
@@ -17,7 +17,10 @@ const getMode = (array) => {
     if (new Set(Object.values(counts)).size === 1) {
         return null;
     }
-    const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+    const highest = Object.keys(counts).sort(
+        (a, b) => counts[b] - counts[a]
+    )[0];
+    const mode = Object.keys(counts);
 };
 
 const calculate = () => {
