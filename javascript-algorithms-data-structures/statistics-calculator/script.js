34377af17d7311fe5c2be39b69693546389a6736
Robert Taylor <trobert844@gmail.com>
2025-03-07 10:06:11 -0500

    statistics calculator step 46

--- javascript-algorithms-data-structures/statistics-calculator/script.js
+++ javascript-algorithms-data-structures/statistics-calculator/script.js
@@ -26,7 +26,9 @@ const getMode = (array) => {
     return mode.join(", ");
 };
 
-const getRange = (array) => Math.max(...array) - Math.min(...array);
+const getRange = (array) => {
+    return Math.max(...array) - Math.min(...array);
+}
 
 const calculate = () => {
     const value = document.querySelector("#numbers").value;
@@ -36,8 +38,10 @@ const calculate = () => {
     const mean = getMean(numbers);
     const median = getMedian(numbers);
     const mode = getMode(numbers);
+    const range = getRange(numbers);
 
     document.querySelector("#mean").textContent = mean;
     document.querySelector("#median").textContent = median;
     document.querySelector("#mode").textContent = mode;
+    document.querySelector("#range").textContent = range;
 };
