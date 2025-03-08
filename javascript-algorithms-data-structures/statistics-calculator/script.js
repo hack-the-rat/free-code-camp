79910ee115798d89cc8bb418721f95f7af000cf1
Robert Taylor <trobert844@gmail.com>
2025-03-08 17:02:30 -0500

    statistics calculator step 57

--- javascript-algorithms-data-structures/statistics-calculator/script.js
+++ javascript-algorithms-data-structures/statistics-calculator/script.js
@@ -42,7 +42,7 @@ const getVariance = (array) => {
 
 const getStandardDeviation = (array) => {
     const variance = getVariance(array);
-    const standardDeviation = Math.pow(variance, 0.5);
+    const standardDeviation = Math.sqrt(variance);
 };
 
 const calculate = () => {
