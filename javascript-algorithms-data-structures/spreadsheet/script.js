c5070ddcc844fe4217fd730302063cab4bcdf7d7
Robert Taylor <trobert844@gmail.com>
2025-05-04 17:11:15 -0400

    spreadsheet step 21

--- javascript-algorithms-data-structures/spreadsheet/script.js
+++ javascript-algorithms-data-structures/spreadsheet/script.js
@@ -1,3 +1,4 @@
+const isEven = num => num % 2 === 0;
 const sum = nums => nums.reduce((acc, el) => acc + el, 0);
 
 const range = (start, end) => Array(end - start + 1).fill(start).map((element, index) => element + index);
